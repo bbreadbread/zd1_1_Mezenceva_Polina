e16d8dad73e8c45639f464122ebbcf15dcacf0bf
Mezenceva Polina <breadtt00@gmail.com>
2024-11-02 12:54:08 +0500

    Добавьте файлы проекта.

---
+++ CatClass.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YP_1
+{
+    class Cat
+    {
+        private string name; //создаем поле имя
+        private double weight; //создаем поле вес
+
+        public string Name // свойство
+        {
+            // получение значения - просто возврат name
+            get { return name; }
+            // установка значения - используем проверку
+            set
+            {
+                bool OnlyLetters = true;
+                // ключ. слово value - это то, что хотят свойству присвоить
+                foreach (var ch in value)
+                {
+                    if (!char.IsLetter(ch))
+                    {
+                        OnlyLetters = false;
+                    }
+                }
+
+                if (OnlyLetters)
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - неправильное имя!!!");
+                }
+            }
+        }
+
+        public double Weight // свойство
+        {
+            // получение значения - просто возврат weight
+            get { return weight; }
+            // установка значения - используем проверку
+            set
+            {
+                weight = value;
+            }
+        }
+
+        public Cat(string CatName) //добавляем констуктор
+        {
+            Name = CatName;
+        }
+
+        public void Meow() // АААААА Я В ТАЗИКЕ ЖЕСТЬ МЯЯЯЯУ!!!!
+        {
+            Console.WriteLine($"{name}: АААААА Я В ТАЗИКЕ ЖЕСТЬ МЯЯЯЯУ!!!!");
+        }
+        public void GetWei() // получаем вес
+        {
+            Console.WriteLine($"Вес кота {name}а: {weight}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using YP_1;
+
+class Program
+{
+    static void Main(string[] args) // основная программа
+    {
+        Cat murzik = new Cat("Мурзик");
+        Cat barsik = new Cat("Барсик");
+        Cat rizhic = new Cat("Рыжик");
+
+        murzik.Meow();//мяукаем(одном котом)
+        barsik.Meow();//мяукаем(другим котом)
+
+        Console.WriteLine();
+
+        barsik.Name = "Барсичечка";//правильный ввод, присваиваем полю новое имя через set
+        barsik.Meow();
+
+        Console.WriteLine();
+
+        barsik.Name = "1234";//НЕправильный ввод, присваиваем полю новое имя через set
+        barsik.Meow();
+
+        Console.WriteLine();
+
+        Console.WriteLine("Вес Рыжика:");
+        var catWei = Console.ReadLine();//ввод потенциального веса кота
+
+        if (double.TryParse(catWei.ToString(), out double bb)) //проверка на ввод
+        {
+            if (bb > 0) //вес не может быть меньше 0
+            {
+                rizhic.Weight = bb;//присваивание коту веса
+                rizhic.GetWei();//вывод
+            }
+            else { Console.WriteLine($"{catWei} - неправильный вес!!!"); }
+        }
+        else { Console.WriteLine($"{catWei} - неправильный вес!!!"); }
+    }
+}
